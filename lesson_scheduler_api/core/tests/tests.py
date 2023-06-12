4d1ebf36b3a902886fdea7fd7f2f443417eaca8d
Mohammed Akmal Miah <mohammedmiah67890@gmail.com>
2023-06-12 09:25:08 +0100

    testcase fix and docker command fix

--- lesson_scheduler_api/core/tests/tests.py
+++ lesson_scheduler_api/core/tests/tests.py
@@ -12,25 +12,21 @@ class CreateBookingViewTestCase(TestCase):
         self.factory = APIRequestFactory()
         self.view = CreateBookingView.as_view({"post": "create_booking"})
         self.user = User.objects.create(username="testuser", password="testpassword")
-        self.booking_data = {}
+        self.booking_data = {"lesson": "wrestling"}
+        self.headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+        }
 
     def test_create_booking(self):
         url = "/core/create_booking/?id={}".format(self.user.id)
-        headers = {
-            "accept": "application/json",
-            "Content-Type": "application/json",
-            "X-CSRFTOKEN": "",
-        }
-        data = {
-            "lesson": "wrestling",
-        }
-        request = self.factory.post(url, data=json.dumps(data), content_type="application/json")
-        request.META["HTTP_ACCEPT"] = headers["accept"]
-        request.META["HTTP_X_CSRFTOKEN"] = headers["X-CSRFTOKEN"]
+
+        request = self.factory.post(url, data=json.dumps(self.booking_data), content_type="application/json")
+        request.META["HTTP_ACCEPT"] = self.headers["accept"]
         force_authenticate(request, user=self.user)
         response = self.view(request)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Booking.objects.count(), 1)
         self.assertEqual(Booking.objects.first().user, self.user)
-        self.assertEqual(Booking.objects.first().lesson, data["lesson"])
+        self.assertEqual(Booking.objects.first().lesson, self.booking_data["lesson"])
